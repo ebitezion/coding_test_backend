4c1978a5763ecb78f6aa509933ecd1645c3089b1
Ebite Zion <ebitezion@gmail.com>
2025-05-25 17:19:36 +0100

    updated seed

--- .history/prisma/seed_20250523195529.ts
+++ .history/prisma/seed_20250523195529.ts
@@ -4,15 +4,15 @@ const prisma = new PrismaClient();
 const mockRestaurants = [
   {
     id: "4dc2e1d1-fe89-4a29-b86a-f8bb0ce1395d",
-    name: "Kagurazaka Ishikawa",
+    name: "Kagurazaka Ishikawa Sushi Haru Nakanoshima Sushi",
     desc: "Enjoy the highest quality Omakase with unlimited sake at a reasonable price.",
     category: "YAKITORI",
     city: "osaka",
     rating: 4.2,
     ratingCount: 139,
-    priceRange: "3000~5000",
+    priceRange: "3~5",
     images: [
-      "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?q=75&fm=jpg&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1555396273-36734d41d205?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=75&fm=jpg&w=1080&fit=max"
     ],
@@ -28,9 +28,9 @@ const mockRestaurants = [
     city: "tokyo",
     rating: 4.5,
     ratingCount: 200,
-    priceRange: "4000~6000",
+    priceRange: "4~6",
     images: [
-      "https://images.unsplash.com/photo-1541544741938-0af808871cc0?q=75&fm=jpg&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1514933651103-005eec06c04b?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1552566626-52f8b828add9?q=75&fm=jpg&w=1080&fit=max"
     ],
@@ -46,15 +46,15 @@ const mockRestaurants = [
     city: "kyoto",
     rating: 4.7,
     ratingCount: 180,
-    priceRange: "2000~4000",
+    priceRange: "2~4",
     images: [
-      "https://images.unsplash.com/photo-1528605248644-14dd04022da1?q=75&fm=jpg&w=1080&fit=max",
+      "https://images.unsplash.com/photo-1513161455079-7f7b6c960e0d?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1559339352-11d035aa65de?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1578474846511-04ba529f3d32?q=75&fm=jpg&w=1080&fit=max"
     ],
     featuredText: "Kyoto’s Famous Ramen Spot",
     featuredIcon: "stars-02",
-    isFavorite: false
+    isFavorite: true
   },
   {
     id: "8ce5g4f3-jg09-7c40-e98c-i1ee3ff3408f",
@@ -64,7 +64,7 @@ const mockRestaurants = [
     city: "nagoya",
     rating: 4.3,
     ratingCount: 220,
-    priceRange: "3000~5000",
+    priceRange: "3~5",
     images: [
       "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=75&fm=jpg&w=1080&fit=max",
@@ -82,7 +82,7 @@ const mockRestaurants = [
     city: "fukuoka",
     rating: 4.6,
     ratingCount: 190,
-    priceRange: "2000~4000",
+    priceRange: "2~4",
     images: [
       "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=75&fm=jpg&w=1080&fit=max",
       "https://images.unsplash.com/photo-1565958011703-44f9829ba187?q=75&fm=jpg&w=1080&fit=max",
